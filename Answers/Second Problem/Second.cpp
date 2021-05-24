755f928cbf4069da1d6498d242dde665dc824765
Julio Arita Torres <julioarita@Julios-MacBook-Pro.local>
2021-05-23 23:26:40 -0300

    Adding an example to the sixth problem. Try to backup my response

--- Answers/Second Problem/Second.cpp
+++ Answers/Second Problem/Second.cpp
@@ -2,7 +2,7 @@
 
 Julio Arita 21/05/2021
 
-I used a simple 'for' loop. I just look at the remainder, if it is equal
+I used a simple 'for' loop. I just look at the quotient, if it is equal
 to zero, then the number is a multiple.
 
 **************************************************************************/
